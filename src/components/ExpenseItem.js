84d71a6a915c5599bf07fdfa205a777b5ccd5bc5
Sumen Kumar Dubey <sumenkumard@gmail.com>
2024-03-25 15:52:15 +0530

    Added Javascript inside JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,21 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(){
-    return (
-      <div className='expense-item'>
-      <div className='expense-item__description'>15 August 2023</div>
-      <div>
-        <h2>Book</h2>
-        <div className='expense-item__price'>$10</div>
-        <div className='expense-item__location'>Delhi</div>
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 7, 15).toISOString();
+  const expenseTitle = "Insurance";
+  const expenseAmount = 50;
+  const location = "Bangalore";
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__location">{location}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
-    )
-  }
-  
-  export default ExpenseItem;
+  );
+}
+
+export default ExpenseItem;
