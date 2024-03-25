600dd41506fe682bb5051e3f4796f5c27faba66e
Sumen Kumar Dubey <sumenkumard@gmail.com>
2024-03-25 16:37:40 +0530

    Used Map, and learned the use of map() and forEach()

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
+// Write your code at the relevant places in the code below:
+
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id: "1",location: "Bangalore", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
+    { id: "2",location: "Delhi", date: new Date(2023, 3, 25), title: "Book", price: 10 },
+    { id: "3",location: "Hyderabad", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
+    { id: "4",location: "Mumbai", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
+  ];
+
   return (
     <div>
       <h1>Let's get Started</h1>
-      <ExpenseItem
-        date={new Date(2023, 7, 15)}
-        title="Insurance"
-        price="50"
-        location="Bangalore"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 3, 25)}
-        title="Book"
-        price="20"
-        location="Delhi"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 10, 11)}
-        title="Pen"
-        price="5"
-        location="Hyderabad"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 1, 14)}
-        title="Laptop"
-        price="200"
-        location="Mumbai"
-      ></ExpenseItem>
+      {expenses.map((expense, index) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            date={expense.date}
+            title={expense.title}
+            price={expense.price}
+            location={expense.location}
+          ></ExpenseItem>
+        );
+      })}
     </div>
   );
 }
