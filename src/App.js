5e7bd194027ff267963380f10bb43ce3e85fb081
Sumen Kumar Dubey <sumenkumard@gmail.com>
2024-03-26 08:52:41 +0530

    Made the Date String , looking like a calender

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const expenses = [
     { id: "1",location: "Bangalore", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
     { id: "2",location: "Delhi", date: new Date(2023, 3, 25), title: "Book", price: 10 },
-    { id: "3",location: "Hyderabad", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
+    { id: "3",location: "Hyderabad", date: new Date(2023, 10, 1), title: "Pen", price: 1 },
     { id: "4",location: "Mumbai", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
   ];
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,23 @@
 // Write your code at the relevant places in the code below:
 
 import "./ExpenseItem.css";
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function ExpenseItem(props) {
+  let date = props.date.getDate().toString();
+  const month = months[Number(props.date.getMonth())];
+  const year = props.date.getFullYear();
+  console.log(date, month, year);
+  date = date.length === 1 ? "0" + date : date;
+  console.log("hello",date, month, year);
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{date}</div>
+      </div>
       <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
